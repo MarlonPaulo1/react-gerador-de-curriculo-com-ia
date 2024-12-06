7b69bdaddd565aea74d854f34fb23acaa745a51d
Marlon Paulo <marlonpaulo1@gmail.com>
2024-12-06 19:27:32 -0300

    Setup do projeto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      Hello World!
+      <Button>Click Aqui!</Button>
+    </>
+  )
+}
+
+export default App
